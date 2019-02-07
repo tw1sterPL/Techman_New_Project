f8cca7fa4175845c3dfed32f023dfe89d1f3075e
Lukas Gorski <xxgambrynusxx@gmail.com>
2019-02-07 17:28:03 +0100

    section: main

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-// Preloader by jQuery
-$(window).on("load",function() {
+// Preloader
+$(window).on("load", function () {
     setTimeout(
-        function() {
+        function () {
             $(".first-screen").fadeOut();
         }, 3000)
 });
-// Menu hamburger by jQuery
+// Menu hamburger
 $(".burger").on("click", function () {
     $(".fas,aside,.burger").toggleClass("show");
+});
+
+//Typed Text
+$(function () {
+    new Typed('#typed', {
+        strings: ["OKNA", "DRZWI", "ROLETY", "BRAMY", "WERTIKALE"],
+        typeSpeed: 100,
+        backDelay: 1500,
+        loop: true,
+        callback: function () {}
+    });
 });
