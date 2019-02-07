9374d8dec322fa102b7f1d3e2843429723b30fdd
Lukas Gorski <xxgambrynusxx@gmail.com>
2019-02-07 15:32:28 +0100

    preloader
    add preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Preloader by jQuery
+$(window).on("load",function() {
+    setTimeout(
+        function() {
+            $(".first-screen").fadeOut();
+        }, 3000)
+});
 // Menu hamburger by jQuery
 $(".burger").on("click", function () {
     $(".fas,aside,.burger").toggleClass("show");
